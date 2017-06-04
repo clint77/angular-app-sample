b073390a1920361cd4c227d79a9f770ffba25dab
Clint <clintp1@gmail.com>
2017-06-04 16:25:05 +1000

    added methods to server: get put and delete

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
     process.exit(1);
   }
 
-    // Save database object from the callback for reuse.
+  // Save database object from the callback for reuse.
   db = database;
   console.log("Database connection ready");
 
@@ -45,6 +45,13 @@ function handleError(res, reason, message, code) {
  */
 
 app.get("/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post("/contacts", function(req, res) {
@@ -71,12 +78,35 @@ app.post("/contacts", function(req, res) {
  */
 
 app.get("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contact");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
 });
 
 app.put("/contacts/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update contact");
+    } else {
+      res.status(204).end();
+    }
+  });
 });
 
 app.delete("/contacts/:id", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete contact");
+    } else {
+      res.status(204).end();
+    }
+  });
 });
-
 
